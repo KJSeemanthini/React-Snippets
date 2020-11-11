3fda7ff029f050f29ce6a9756a63dd3762521b26
KJSeemanthini <seema.96@gmail.com>
2020-11-11 09:12:13 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Icon, initializeIcons } from "office-ui-fabric-react";
+
+import "./styles.css";
+initializeIcons();
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+  const decrementCounter = () => {
+    setCounter(counter - 1);
+  };
+  return (
+    <div className="Tile">
+      <Icon
+        className="Tile__subtract"
+        iconName="CalculatorSubtract"
+        title={"Decrement Count"}
+        onClick={decrementCounter}
+        style={{
+          color: "#ec4444",
+          fontWeight: "bold",
+          fontSize: "19px",
+          height: "20px",
+          padding: "3px"
+        }}
+      />
+
+      <div className="Tile__body">{counter}</div>
+
+      <Icon
+        className="Tile__add"
+        iconName="Add"
+        title={"Increment count"}
+        onClick={incrementCounter}
+        style={{
+          color: "rgba(74, 172, 28, 0.925)",
+          height: "20px",
+          fontWeight: "bold",
+          fontSize: "19px",
+          padding: "3px"
+        }}
+      />
+    </div>
+  );
+}
